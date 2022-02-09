f4e6392013de884e95f735224f716099ed447ddc
Brunelle <bmalandila@yahoo.fr>
2022-02-09 16:13:08 +0100

    Added content

---
+++ backend-orchestration/app.js
@@ -0,0 +1,77 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var cors = require('cors')
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
+
+const session = require('express-session');
+
+app.use(
+  session({
+      secret: 'secret string',
+      resave: false,
+      saveUninitialized: false,
+      cookie: { /* can add cookie related info here */ }
+  })
+);
+
+app.get('/home', function(req, res){
+  if(!req.session.pageCountByCurrentUserOrAnyNameYouWant)
+      req.session.pageCountByCurrentUserOrAnyNameYouWant = 0;
+  req.session.pageCountByCurrentUserOrAnyNameYouWant++;
+  console.log(req.session.pageCountByCurrentUserOrAnyNameYouWant)
+  res.send({
+      pageCount: req.session.pageCountByCurrentUserOrAnyNameYouWant
+  });
+});
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+
+var PORT = 5000;
+ 
+app.listen(PORT, function(err){
+    if (err) console.log("Error in server setup")
+    console.log("Server listening on Port", PORT);
+})
+
+
+      
+module.exports = app;
